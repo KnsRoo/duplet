8ea15d850cb147c35c37122822af908c551c59ec
ilya_orishin <knsroo@gmail.com>
2021-01-11 12:38:16 +0300

    fix

--- src/vue/news/new-item.vue
+++ src/vue/news/new-item.vue
@@ -1,11 +1,11 @@
 <template lang = "pug">
 .news__card
-	img.news__card_img(src='newsItem.picture' alt='')
+	img.news__card_img(:src="newsItem.picture" alt='')
 	.news__card_title {{ newsItem.title }}
 	.news__card_read
 		a.news__card_read_title(:href='newsItem.pageRef') Читать
 		.news__card_read_data
-			img.news__card_read_data_svg(:src="newsItem.picture" alt='')
+			img.news__card_read_data_svg(src="/assets/img/icons/data.svg" alt='')
 			.news__card_read_data_number {{ format(newsItem.creationDate) }}
 </template>
 
